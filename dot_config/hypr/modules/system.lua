6f6bd50d109d684f6747f5cdad976559e91076e3
EFrMG <204592755+EFrMG@users.noreply.github.com>
2026-08-18 13:54:45 -0300

    bin: OCR script that shows the full text extracted in a notification
    uwsm: removed Omarchy-specific envs and put them in Hyprland's scope

--- dot_config/hypr/modules/system.lua
+++ dot_config/hypr/modules/system.lua
@@ -7,3 +7,6 @@ hl.config({
     scroll_event_delay = 100
   },
 })
+
+hl.env("OMARCHY_SCREENSHOT_DIR", os.getenv("HOME") .. "/Pictures/captures")
+hl.env("OMARCHY_SCREENRECORD_DIR", os.getenv("HOME") .. "/Videos/OBS")
